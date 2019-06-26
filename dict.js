96ac4c93854e5b51651294dc5c269f0c00e52a8c
Victore Anonymo Minore <52244338+VictoreAnonymo@users.noreply.github.com>
2019-06-26 14:18:40 +0200

    Add files via upload

---
+++ dict.js
@@ -0,0 +1,474 @@
+var dict = [
+	{
+		en: "against", LM: "contra",
+		examples: []
+	},
+	{
+		en: "alcohol", LM: "alcohole",
+		examples: []
+	},
+	{
+		en: "alone", LM: "solo",
+		examples: []
+	},
+	{
+		en: "always", LM: "semper",
+		examples: []
+	},
+	{
+		en: "and", LM: "et, atque",
+		examples: [
+			"The boy and the girl are playing. → Puero et puella lude.",
+			"I go to school and you stay here. → Me i ad schola atque te resta hic."
+		]
+	},
+	{
+		en: "animal", LM: "animale",
+		examples: []
+	},
+	{
+		en: "apple", LM: "malo",
+		examples: []
+	},
+	{
+		en: "art", LM: "arte",
+		examples: []
+	},
+	{
+		en: "be", LM: "esse/es",
+		examples: []
+	},
+	{
+		en: "bed", LM: "lecto",
+		examples: []
+	},
+	{
+		en: "become", LM: "fieri/fi",
+		examples: ["I'm whitening the shirt. → Me fi albo tunica."]
+	},
+	{
+		en: "believe", LM: "credere",
+		examples: []
+	},
+	{
+		en: "bird", LM: "ave",
+		examples: []
+	},
+	{
+		en: "blood", LM: "sanguine",
+		examples: []
+	},
+	{
+		en: "body", LM: "corpore",
+		examples: []
+	},
+	{
+		en: "book", LM: "libro",
+		examples: []
+	},
+	{
+		en: "brief", LM: "breve",
+		examples: []
+	},
+	{
+		en: "can", LM: "posse/pote",
+		examples: ["I cannot sleep. → Me non pote dormi."]
+	},
+	{
+		en: "cat", LM: "fele",
+		examples: []
+	},
+	{
+		en: "clean state", LM: "tabula rasa",
+		examples: []
+	},
+	{
+		en: "cloud", LM: "nube",
+		examples: []
+	},
+	{
+		en: "conduct", LM: "ducere",
+		examples: []
+	},
+	{
+		en: "conquere", LM: "vincere",
+		examples: []
+	},
+	{
+		en: "contain", LM: "continere",
+		examples: []
+	},
+	{
+		en: "cube", LM: "cubo",
+		examples: []
+	},
+	{
+		en: "day", LM: "die",
+		examples: []
+	},
+	{
+		en: "die", LM: "morere",
+		examples: []
+	},
+	{
+		en: "divide", LM: "divide",
+		examples: []
+	},
+	{
+		en: "dog", LM: "cane",
+		examples: []
+	},
+	{
+		en: "dragon", LM: "dracone",
+		examples: []
+	},
+	{
+		en: "drink", LM: "bibere",
+		examples: []
+	},
+	{
+		en: "during", LM: "per",
+		examples: []
+	},
+	{
+		en: "eat", LM: "edere",
+		examples: []
+	},
+	{
+		en: "exception", LM: "exceptione",
+		examples: []
+	},
+	{
+		en: "explain", LM: "explicare",
+		examples: []
+	},
+	{
+		en: "Europe", LM: "Europa",
+		examples: []
+	},
+	{
+		en: "fake money", LM: "lupino",
+		examples: []
+	},
+	{
+		en: "father", LM: "patre",
+		examples: []
+	},
+	{
+		en: "fly", LM: "volare, musca",
+		examples: [
+			"Birds fly. → Aves vola.",
+			"I hate flies. → Me odi muscas."
+		]
+	},
+	{
+		en: "foot", LM: "pede",
+		examples: []
+	},
+	{
+		en: "France", LM: "Francia",
+		examples: []
+	},
+	{
+		en: "friend", LM: "amico",
+		examples: []
+	},
+	{
+		en: "hand", LM: "manu",
+		examples: []
+	},
+	{
+		en: "have", LM: "habere",
+		examples: []
+	},
+	{
+		en: "hate", LM: "odire",
+		examples: []
+	},
+	{
+		en: "heart", LM: "corde",
+		examples: []
+	},
+	{
+		en: "hello", LM: "ave, salve",
+		examples: []
+	},
+	{
+		en: "horse", LM: "equo",
+		examples: ["Caligula's horse is consul. → Equo de Caligula es consule."]
+	},
+	{
+		en: "house", LM: "domo",
+		examples: []
+	},
+	{
+		en: "idiot", LM: "idiota",
+		examples: []
+	},
+	{
+		en: "jump", LM: "saltare",
+		examples: []
+	},
+	{
+		en: "keyboard", LM: "claviatura",
+		examples: []
+	},
+	{
+		en: "king", LM: "rege",
+		examples: []
+	},
+	{
+		en: "knife", LM: "cultro",
+		examples: []
+	},
+	{
+		en: "look", LM: "spectare, parere",
+		examples: [
+			"Look! → Specta!",
+			"You look sage. → Te pare sapiente."
+		]
+	},
+	{
+		en: "love", LM: "amare",
+		examples: []
+	},
+	{
+		en: "milk", LM: "lacte",
+		examples: []
+	},
+	{
+		en: "money", LM: "moneta",
+		examples: []
+	},
+	{
+		en: "mother", LM: "matre",
+		examples: []
+	},
+	{
+		en: "music", LM: "musica",
+		examples: []
+	},
+	{
+		en: "nose", LM: "naso",
+		examples: []
+	},
+	{
+		en: "now", LM: "nunc",
+		examples: []
+	},
+	{
+		en: "obey", LM: "obedire",
+		examples: []
+	},
+	{
+		en: "obscure", LM: "obscuro",
+		examples: []
+	},
+	{
+		en: "painting", LM: "pictura",
+		examples: []
+	},
+	{
+		en: "past", LM: "passato",
+		examples: []
+	},
+	{
+		en: "peace", LM: "pace",
+		examples: []
+	},
+	{
+		en: "proove", LM: "probare",
+		examples: []
+	},
+	{
+		en: "progress", LM: "progressu",
+		examples: []
+	},
+	{
+		en: "prophet", LM: "propheta",
+		examples: []
+	},
+	{
+		en: "prudent", LM: "prudente",
+		examples: []
+	},
+	{
+		en: "queen", LM: "regina",
+		examples: []
+	},
+	{
+		en: "read", LM: "legere",
+		examples: []
+	},
+	{
+		en: "religion", LM: "religione",
+		examples: []
+	},
+	{
+		en: "rule", LM: "regula",
+		examples: []
+	},
+	{
+		en: "run", LM: "currere",
+		examples: []
+	},
+	{
+		en: "sad", LM: "triste",
+		examples: []
+	},
+	{
+		en: "sane", LM: "sano",
+		examples: []
+	},
+	{
+		en: "say", LM: "dicere",
+		examples: []
+	},
+	{
+		en: "see", LM: "videre",
+		examples: []
+	},
+	{
+		en: "seize", LM: "carpere",
+		examples: ["Seize the day. → Carpe die."]
+	},
+	{
+		en: "sell", LM: "vendere",
+		examples: []
+	},
+	{
+		en: "send", LM: "mittere",
+		examples: []
+	},
+	{
+		en: "shirt", LM: "tunica",
+		examples: []
+	},
+	{
+		en: "shoe", LM: "calceo",
+		examples: []
+	},
+	{
+		en: "sky", LM: "caelo",
+		examples: []
+	},
+	{
+		en: "sleep", LM: "dormire",
+		examples: []
+	},
+	{
+		en: "soldier", LM: "milite",
+		examples: []
+	},
+	{
+		en: "speak", LM: "loquere",
+		examples: []
+	},
+	{
+		en: "sport", LM: "ludo",
+		examples: []
+	},
+	{
+		en: "spring", LM: "ver",
+		examples: []
+	},
+	{
+		en: "stone", LM: "lapide",
+		examples: []
+	},
+	{
+		en: "sweet", LM: "dulce",
+		examples: []
+	},
+	{
+		en: "table", LM: "tabula",
+		examples: []
+	},
+	{
+		en: "territory", LM: "terra",
+		examples: []
+	},
+	{
+		en: "theory", LM: "theoria",
+		examples: []
+	},
+	{
+		en: "teeth", LM: "dente",
+		examples: []
+	},
+	{
+		en: "today", LM: "hodie",
+		examples: []
+	},
+	{
+		en: "tomorrow", LM: "cras",
+		examples: []
+	},
+	{
+		en: "tongue", LM: "lingua",
+		examples: []
+	},
+	{
+		en: "t-shirt", LM: "tunicula",
+		examples: []
+	},
+	{
+		en: "uncle", LM: "patruo",
+		examples: []
+	},
+	{
+		en: "unicorn", LM: "unicorne",
+		examples: []
+	},
+	{
+		en: "use", LM: "utere",
+		examples: []
+	},
+	{
+		en: "utopia", LM: "utopia",
+		examples: []
+	},
+	{
+		en: "value", LM: "valore",
+		examples: []
+	},
+	{
+		en: "weapon", LM: "arma",
+		examples: []
+	},
+	{
+		en: "want", LM: "volle/vol",
+		examples: ["I want to sleep. → Me vol dormi."]
+	},
+	{
+		en: "war", LM: "bello",
+		examples: []
+	},
+	{
+		en: "water", LM: "aqua",
+		examples: []
+	},
+	{
+		en: "while", LM: "dum",
+		examples: ["I speak while running. → Me loque dum currente."]
+	},
+	{
+		en: "white", LM: "albo",
+		examples: []
+	},
+	{
+		en: "who", LM: "qui",
+		examples: [
+			"Who ate the apple? → Qui e ede malo?",
+			"He's the man who ate an apple → Illo es homo qui e ede malo."
+		]
+	},
+	{
+		en: "with", LM: "cum",
+		examples: []
+	},
+	{
+		en: "wolf", LM: "lupo",
+		examples: []
+	},
+];
